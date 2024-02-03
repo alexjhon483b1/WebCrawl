ff25f4779fd8badba493642d1f19257abacba422
petdoe <hw3952877@gmail.com>
2024-02-03 20:15:58 +0600

    3rd

--- main.sh
+++ main.sh
@@ -8,26 +8,9 @@ fi
 
 domain=$1
 
-# Run your program with the provided domain and capture the output
-output=$(./checkssl "$domain" 2) 
-
-# Get the last line using awk
-last_line=$(echo "$output")
-
-# Now you can check or use the last line as needed
-echo "$last_line"
-
-if [ "$last_line" = "HTTPS" ]; then
  ./ssl "$domain">>x.html  # Correct syntax for calling another script
  url="https://$domain"
- python3 scrap.py "$url"
+ python3 scrap.py "$url" 
  rm x.html
-fi
 
-if [ "$last_line" = "HTTP" ]; then
- get=$(./get "$domain")>>x.html  # Correct syntax for calling another script
- url="https://$domain"
- python3 scrap.py "$url"
- rm x.html
-fi
 
--- scrap.py
+++ scrap.py
@@ -8,7 +8,6 @@ def extract_links(html_content, domain):
     
     for link in links:
         href = link['href']
-        parsed_domain = urlparse(domain)
         parsed_url = urlparse(href)
         if not href.startswith(('http://', 'https://')):
             # Relative link, make it absolute using the specified domain
@@ -21,11 +20,33 @@ def extract_links(html_content, domain):
             if not href.startswith(('http://', 'https://')):
                    if not href.startswith(('//', '//')):
                             link_list.append(parsed_domain.scheme+"://"+href)
+                   if not href.startswith(('/', '/')):
+                            link_list.append(parsed_domain.scheme+"://"+parsed_domain.netloc+"/"+href)
                    else: 
                           link_list.append(parsed_domain.scheme+":"+href)       
             if href.startswith(('http://', 'https://')):
                           link_list.append(href)
 
+
+def extract_path(html_content, ndomain):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    links = soup.find_all('a', href=True)   
+    for link in links:
+        href = link['href']
+        parsed_url = urlparse(href)
+        if href.startswith(('http://', 'https://')):
+              if parsed_url.netloc == parsed_domain.netloc:
+                   link_list.append(href)
+        if not href.startswith(('http://', 'https://')): 
+               if parsed_url.netloc == parsed_domain.netloc:
+                       if href.startswith(('//', '//')):
+                             link_list.append(parsed_domain.scheme+":"+href)    
+                       if not href.startswith(('/', '/')):
+                            link_list.append(parsed_domain.scheme+"://"+parsed_domain.netloc+parsed_domain.path+"/"+href)
+                       else:
+                             link_list.append(parsed_domain.scheme+":"+href)
+                            
+               
 if __name__ == "__main__":
     import sys
 
@@ -37,10 +58,19 @@ if __name__ == "__main__":
           html_x = file.read()
     html_content = html_x
     domain = sys.argv[1]
-
-    extract_links(html_content, domain)
+    parsed_domain = urlparse(domain)
+    if parsed_domain.path != "":
+           ndomain = parsed_domain.netloc
+           extract_path(html_content, ndomain)
+    if parsed_domain.path == "":
+            extract_links(html_content, domain)
     unique_list = list(set(link_list))
     if domain in unique_list:
         unique_list.remove(domain)
-        print(unique_list)
+    a =parsed_domain.scheme+"://"+parsed_domain.netloc+"/"
+    if a in unique_list:
+        unique_list.remove(a)
+    for item in unique_list:
+        if item != '' and item != ',':
+              print(item)
 
